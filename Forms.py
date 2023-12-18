d1f7d85b177fb21c5a4d597e81b3ebbca7f25ee3
Rachel Chee <rachelcheelz@gmail.com>
2023-12-18 11:32:45 +0800

    create seller product form

--- Forms.py
+++ Forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, FileField, TextAreaField, IntegerField, FloatField, validators
 from wtforms.validators import DataRequired
 from wtforms.fields import EmailField
 
@@ -9,3 +9,11 @@ class CreateUserForm(Form):
                            validators=[DataRequired(),
                                        validators.Length(min=8, max=30),
                                        validators.Regexp(regex="[a-zA-Z0-9]")])
+
+
+class CreateProductForm(Form):
+    product_name = StringField('Product Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    product_price = FloatField('Price', validators.DataRequired())
+    product_stock = IntegerField('Stock', validators.DataRequired())
+    image = FileField('Image File', [validators.regexp('^[^/\\]\.jpg$'), validators.DataRequired()])
+    description = TextAreaField('Product Description')
