7a3820d4035116347d6659ddc021e9c4792187f1
May Than Hsu Aung <maythanhsuaung1111@gmail.com>
2023-12-20 14:58:23 +0800

    added approved applicationforms

---
+++ static/JS/components.js
@@ -0,0 +1,4 @@
+// approved_btn = document.getElementById("approved_btn");
+// approved_btn.addEventListener("click", function () {
+//     confirm("Are you sure you want to approve this application?");
+// });
