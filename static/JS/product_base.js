1457192f78acd70181e62d4af2c03728072b6fc0
Jun Yuan <wolftoast529@gmail.com>
2024-01-06 21:47:34 +0800

    Updated Shopping Cart with more Functions

--- static/JS/product_base.js
+++ static/JS/product_base.js
@@ -6,13 +6,27 @@ $("document").ready(function(){
 
     //Increment of Product
     $("#increment").click(function() {
-        var product_qty = $("#qty-count").text();
-        product_qty = parseInt(product_qty);
-        product_qty += 1;
-        $("#qty-count").text(product_qty);
+        //Fetching Product Available Qty
+        $.ajax({
+            url: '',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "request_type" : "product_stock",
+            }),
+            success: function(response) {
+                var product_stock = response.stock
+                var product_qty = $("#qty-count").text();
+                product_qty = parseInt(product_qty);
+                if (product_qty < product_stock) {
+                    product_qty += 1;
+                    $("#qty-count").text(product_qty);
 
-        var product_price_update = product_qty * product_price;
-        $("#price").text('$' + product_price_update.toFixed(2));
+                    var product_price_update = product_qty * product_price;
+                    $("#price").text('$' + product_price_update.toFixed(2));
+                }
+            }
+        })
     })
 
     //Decrement of Product
@@ -48,7 +62,11 @@ function addToCart(product_id, seller_id, seller) {
             if (response.result) {
                 $("#cart-item-qty").text(response.data)
             } else {
-                $(".content").text("FAIL")
+                if (response.reason == "added more than stock") {
+                    alert("You have added more than the stock available");
+                } else {
+                    $(".content").text("FAIL");
+                }
             }
 
             }
