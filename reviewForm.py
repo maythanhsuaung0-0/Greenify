3b0883270b3c5c601a392d418877bc7ba0071b12
Rachel Chee <rachelcheelz@gmail.com>
2024-01-15 17:47:22 +0800

    creating ratings and reviews

--- reviewForm.py
+++ reviewForm.py
@@ -1,4 +1,6 @@
 from wtforms import Form, TextAreaField, validators
 
+
 class CreateReviewsForm(Form):
-    review = TextAreaField('My review', [validators.DataRequired()])
+    review = TextAreaField('My Review:', [validators.DataRequired()], render_kw={'rows': '4', 'style': 'width: 550px;', 'placeholder': 'Enter your review here'})
+
