54f6ecc400c381d4fb372ed087def76fb8c6cb9e
Rachel Chee <rachelcheelz@gmail.com>
2024-02-03 21:55:30 +0800

    minor changes
    will add in chatbot soon maybe tmr or after tuesday :) praying that it works

--- __init__.py
+++ __init__.py
@@ -75,36 +75,6 @@ def get_initial_reviews(seller_id, product_id):
     # Get the list of reviews for the product
     return seller_reviews.get(product_id, [])
 
-# testing image upload
-# @app.route('/uploadimage')
-# def base():
-#     return render_template('index.html')
-#
-#
-# @app.route('/uploadimage', methods=['POST'])
-# def upload_image():
-#     if 'file' not in request.files:
-#         flash('No file part')
-#         return redirect(request.url)
-#     file = request.files['file']
-#     if file.filename == '':
-#         flash('No image selected for uploading')
-#         return redirect(request.url)
-#     if file and allowed_file(file.filename):
-#         filename = secure_filename(file.filename)
-#         file.save(os.path.join(app.config['UPLOAD_IMG_FOLDER'], filename))
-#         # print('upload_image filename: ' + filename)
-#         flash('Image successfully uploaded and displayed below')
-#         return render_template('index.html', filename=filename)
-#     else:
-#         flash('Allowed image types are - png, jpg, jpeg, gif')
-#         return redirect(request.url)
-
-
-# @app.route('/display/<filename>')
-# def display_image(filename):
-#     # print('display_image filename: ' + filename)
-#     return redirect(url_for('static', filename='product_image/' + filename), code=301)
 
 
 def delete_folder(item):
